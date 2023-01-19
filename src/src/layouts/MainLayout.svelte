79f25deaf61c7e89d2e6ebbc4b01c40104d4c91e
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-01-19 21:22:06 +0100

    added google analytics

---
+++ src/src/layouts/MainLayout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+	import Analytics from '../components/Analytics.svelte';
+</script>
+
+<Analytics />
+
+<svelte:head>
+	<meta name="theme-color" content="#1f2937" />
+</svelte:head>
+
+<slot />
--- src/src/routes/+page.svelte
+++ src/src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1 class="text-3xl font-bold underline">Hi there</h1>
+<script>
+	import MainLayout from '../layouts/MainLayout.svelte';
+</script>
+
+<MainLayout>
+	<h1 class="text-3xl font-bold underline">Hi there</h1>
+</MainLayout>
