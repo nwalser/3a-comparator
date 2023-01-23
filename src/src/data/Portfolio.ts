bec168ae653e507a0cccc9ff2dacbe2c1ed2f0d7
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-01-23 12:39:57 +0100

    before scraping idea

--- src/src/data/Portfolio.ts
+++ src/src/data/Portfolio.ts
@@ -1,89 +1,47 @@
-export abstract class YearlyChange {
-    abstract calculateYearlyReturns(totalAssets: number): number;
-    abstract calculateYearlyFees(totalAssets: number): number;
-}
+export class Asset {
+    underlyingSecurity: string;
+    relativeAllocation: number;
 
-export abstract class Asset extends YearlyChange {
-    name: string;
-    allocation: number;
-
-    constructor(name: string, allocation: number) {
-        super();
-        this.name = name;
-        this.allocation = allocation;
+    constructor(underlyingSecurity: string, relativeAllocation: number) {
+        this.underlyingSecurity = underlyingSecurity;
+        this.relativeAllocation = relativeAllocation;
     }
 }
 
-export class SecurityAsset extends Asset {
-    annualizedReturns: number;
-    annualizedFees: number;
+export class Allocation {
+    underlyingSecurity: string;
+    absoluteAllocation: number;
 
-    constructor(name: string, allocation: number, annualizedReturns: number, annualizedFees: number = 0) {
-        super(name, allocation);
-        this.annualizedReturns = annualizedReturns;
-        this.annualizedFees = annualizedFees;
-    }
-
-    calculateYearlyReturns(totalAssets: number): number {
-        return totalAssets * this.allocation * this.annualizedReturns;
-    }
-
-    calculateYearlyFees(totalAssets: number): number {
-        return totalAssets * this.allocation * this.annualizedFees;
+    constructor(underlyingSecurity: string, absoluteAllocation: number) {
+        this.underlyingSecurity = underlyingSecurity;
+        this.absoluteAllocation = absoluteAllocation;
     }
 }
 
-abstract class PortfolioFee extends YearlyChange {
-    name: string;
-
-    constructor(name: string) {
-        super();
-        this.name = name;
-    }
-
-    calculateYearlyReturns(totalAssets: number): number {
-        return 0;
-    }
-
-    abstract calculateYearlyFees(totalAssets: number): number;
-}
-
-export class AbsolutePortfolioFee extends PortfolioFee {
-    absoluteFee: number;
-
-    constructor(name: string, absoluteFee: number = 0) {
-        super(name);
-        this.absoluteFee = absoluteFee;
-    }
-
-    calculateYearlyFees(totalAssets: number): number {
-        return this.absoluteFee;
-    }
-}
-
-export class RelativePortfolioFee extends PortfolioFee {
-    relativeFee: number;
-
-    constructor(name: string, relativeFee: number = 0) {
-        super(name);
-        this.relativeFee = relativeFee;
-    }
-
-    calculateYearlyFees(totalAssets: number): number {
-        return totalAssets * this.relativeFee;
-    }
-}
 
 export class Portfolio {
-    provider: string;
-    name: string;
-    fees: PortfolioFee[];
-    assets: Asset[];
+    assets: Asset[] = [];
+    portfolioValue: number;
 
-    constructor(provider: string, name: string, fees: PortfolioFee[] = [], assets: Asset[] = []) {
-        this.provider = provider;
-        this.name = name;
-        this.fees = fees;
-        this.assets = assets;
+    constructor(portfolioValue: number) {
+        this.portfolioValue = portfolioValue;
+    }
+
+    getAssetAllocation(): Allocation[] {
+        return this.assets.map(asset => new Allocation(asset.underlyingSecurity, asset.relativeAllocation * this.portfolioValue));
     }
 }
+
+
+// portfolio end
+
+
+export class Security {
+    name: string;
+    annualPerformance: number;
+    annualFees: number;
+
+    
+}
+
+
--- src/src/routes/new/Sim.ts
+++ src/src/routes/new/Sim.ts
@@ -1,4 +1,93 @@
 
+export abstract class YearlyChange {
+    abstract calculateYearlyReturns(totalAssets: number): number;
+    abstract calculateYearlyFees(totalAssets: number): number;
+}
+
+export abstract class Asset extends YearlyChange {
+    name: string;
+    allocation: number;
+
+    constructor(name: string, allocation: number) {
+        super();
+        this.name = name;
+        this.allocation = allocation;
+    }
+}
+
+export class SecurityAsset extends Asset {
+    annualizedReturns: number;
+    annualizedFees: number;
+
+    constructor(name: string, allocation: number, annualizedReturns: number, annualizedFees: number = 0) {
+        super(name, allocation);
+        this.annualizedReturns = annualizedReturns;
+        this.annualizedFees = annualizedFees;
+    }
+
+    calculateYearlyReturns(totalAssets: number): number {
+        return totalAssets * this.allocation * this.annualizedReturns;
+    }
+
+    calculateYearlyFees(totalAssets: number): number {
+        return totalAssets * this.allocation * this.annualizedFees;
+    }
+}
+
+abstract class PortfolioFee extends YearlyChange {
+    name: string;
+
+    constructor(name: string) {
+        super();
+        this.name = name;
+    }
+
+    calculateYearlyReturns(totalAssets: number): number {
+        return 0;
+    }
+
+    abstract calculateYearlyFees(totalAssets: number): number;
+}
+
+export class AbsolutePortfolioFee extends PortfolioFee {
+    absoluteFee: number;
+
+    constructor(name: string, absoluteFee: number = 0) {
+        super(name);
+        this.absoluteFee = absoluteFee;
+    }
+
+    calculateYearlyFees(totalAssets: number): number {
+        return this.absoluteFee;
+    }
+}
+
+export class RelativePortfolioFee extends PortfolioFee {
+    relativeFee: number;
+
+    constructor(name: string, relativeFee: number = 0) {
+        super(name);
+        this.relativeFee = relativeFee;
+    }
+
+    calculateYearlyFees(totalAssets: number): number {
+        return totalAssets * this.relativeFee;
+    }
+}
+
+export class Portfolio {
+    provider: string;
+    name: string;
+    fees: PortfolioFee[];
+    assets: Asset[];
+
+    constructor(provider: string, name: string, fees: PortfolioFee[] = [], assets: Asset[] = []) {
+        this.provider = provider;
+        this.name = name;
+        this.fees = fees;
+        this.assets = assets;
+    }
+}
 
 type Change = {
     name: string;
